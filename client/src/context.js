fbcb355a40252b9bbf8595ce975ebaa2011d5045
Borislav Atanasov <b_atanasov@hotmail.com>
2019-04-07 05:38:13 +0100

    protected route added

--- client/src/context.js
+++ client/src/context.js
@@ -1,5 +1,5 @@
 import { createContext } from 'react';
 
-const Context = createContext({ currentUser: null });
+const Context = createContext({ currentUser: null, isAuth: null });
 
 export { Context as default };
--- client/src/reducer.js
+++ client/src/reducer.js
@@ -6,6 +6,11 @@ export default function reducer(state, action) {
         ...state,
         currentUser: payload
       };
+    case 'IS_LOGGED_IN':
+      return {
+        ...state,
+        isAuth: payload
+      };
     default:
       return state;
   }
